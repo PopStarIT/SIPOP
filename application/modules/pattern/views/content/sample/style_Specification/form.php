678c523d0043beb469d2a10b3cec3ad8711c3dcf
HarisHandiyana1 <20010014@stmik-mi.ac.id>
2024-06-20 16:06:01 +0700

    test

--- application/modules/pattern/views/content/sample/style_Specification/form.php
+++ application/modules/pattern/views/content/sample/style_Specification/form.php
@@ -28,6 +28,11 @@
 								Image
 							</a>	
 						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="tab_<?php echo $methodid; ?>_picture" data-toggle="tab" href="#content_<?php echo $methodid; ?>_picture" role="tab" aria-controls="content_<?php echo $methodid; ?>_picture" aria-selected="true">
+								Image
+							</a>	
+						</li>
 					</ul>
 					
 					<div class="tab-content">
